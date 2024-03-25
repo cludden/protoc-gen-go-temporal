b82f0b927f463d3675b76e0e19479c76cdd6c3b8
chris ludden <chris.ludden@gmail.com>
2024-03-24 22:48:09 -0600

    feat: adds initial gh pages site

---
+++ docs/sidebars.ts
@@ -0,0 +1,177 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+const sidebars: SidebarsConfig = {
+  // By default, Docusaurus generates a sidebar from the docs folder structure
+  docs: [
+    {
+      type: 'doc',
+      id: 'about',
+      label: 'About'
+    },
+    {
+      type: 'doc',
+      id: 'install',
+      label: 'Installing'
+    },
+    {
+      type: 'doc',
+      id: 'getting-started',
+      label: 'Getting Started'
+    },
+    {
+      type: 'category',
+      label: 'Configuration',
+      items: [
+        {
+          type: 'doc',
+          id: 'configuration/plugin',
+          label: 'Plugin'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/service',
+          label: 'Service'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/workflow',
+          label: 'Workflow'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/activity',
+          label: 'Activities'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/query',
+          label: 'Query'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/signal',
+          label: 'Signal'
+        },
+        {
+          type: 'doc',
+          id: 'configuration/update',
+          label: 'Update'
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Guides',
+      items: [
+        {
+          type: 'doc',
+          id: 'guides/workflows',
+          label: 'Workflows'
+        },
+        {
+          type: 'doc',
+          id: 'guides/activities',
+          label: 'Activities'
+        },
+        {
+          type: 'doc',
+          id: 'guides/queries',
+          label: 'Queries'
+        },
+        {
+          type: 'doc',
+          id: 'guides/signals',
+          label: 'Signals'
+        },
+        {
+          type: 'doc',
+          id: 'guides/testing',
+          label: 'Testing'
+        },
+        {
+          type: 'doc',
+          id: 'guides/updates',
+          label: 'Updates'
+        },
+        {
+          type: 'doc',
+          id: 'guides/clients',
+          label: 'Clients'
+        },
+        {
+          type: 'doc',
+          id: 'guides/cli',
+          label: 'CLI'
+        },
+        {
+          type: 'doc',
+          id: 'guides/child-workflows',
+          label: 'Child Workflows'
+        },
+        {
+          type: 'doc',
+          id: 'guides/xns',
+          label: 'Cross-Namespace (XNS)'
+        },
+        {
+          type: 'doc',
+          id: 'guides/bloblang',
+          label: 'Bloblang Expressions'
+        },
+        {
+          type: 'doc',
+          id: 'guides/codec-server',
+          label: 'Codec Server'
+        },
+        {
+          type: 'doc',
+          id: 'guides/documentation',
+          label: 'Documentation'
+        },
+      ]
+    },
+    {
+      type: 'doc',
+      id: 'reference',
+      label: 'Reference'
+    },
+  ],
+
+  examples: [
+    {
+      type: 'doc',
+      id: 'examples/helloworld',
+      label: 'Hello World'
+    },
+    {
+      type: 'doc',
+      id: 'examples/updatabletimer',
+      label: 'Updatable Timer'
+    }
+  ]
+
+  // But you can create a sidebar manually
+  /*
+  tutorialSidebar: [
+    'intro',
+    'hello',
+    {
+      type: 'category',
+      label: 'Tutorial',
+      items: ['tutorial-basics/create-a-document'],
+    },
+  ],
+   */
+};
+
+export default sidebars;
